215ee910f6b3699b3a31d07aaa7b55ca3a8e3636
Patricia Sirboiu <94383898+patricia43@users.noreply.github.com>
2026-04-26 13:46:11 +0300

    start menu
    starting implementation

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -17,6 +17,11 @@ public class AudioManager : MonoBehaviour
         if (instance == null)
         {
             instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
         }
     }
 
